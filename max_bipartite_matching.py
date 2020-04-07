3fbae3695d329aeb21575d00bd5d333997ec94b2
Sean Gatewood <stg2bd@virginia.edu>
2020-04-06 23:34:15 -0400

    Add limited many-to-many functionality

--- max_bipartite_matching.py
+++ max_bipartite_matching.py
@@ -13,12 +13,28 @@ def check_choices_and_choosers_are_different(sources,destinations):
         if source in destinations:
             raise Exception("Choices cannot have chooser names")
 
-def get_graph(filename):
+def get_limits(info_filename):
+    out = {}
+    if info_filename == None:
+        return {}
+    for lst in maxflow.get_data(info_filename):
+        out[lst[0]] = int(lst[1])
+    return out
+
+def get_graph(filename,chooser_info_filename=None,prize_info_filename=None):
+    """
+    chooser_info_filename: csv file that says how many prizes each person can win
+    prize_info_filename: csv that that says how many people can win each prize
+    Both are defaulted to 1
+    """
 
     graph = maxflow.get_graph(filename)
 
     sources = set()
     destinations = set()
+
+    chooser_limits = get_limits(chooser_info_filename)
+    prize_limits = get_limits(prize_info_filename)
 
     for lst in graph.values():
         for edge in lst:
@@ -28,9 +44,9 @@ def get_graph(filename):
     check_source_and_sink_names(sources.union(destinations))
     check_choices_and_choosers_are_different(sources,destinations)
 
-    graph[SOURCE] = [maxflow.Edge(SOURCE,dest,1) for dest in sources]
+    graph[SOURCE] = [maxflow.Edge(SOURCE,dest,chooser_limits.get(dest,1)) for dest in sources]
     for src in destinations:
-        graph.setdefault(src,[]).append(maxflow.Edge(src,SINK,1))
+        graph.setdefault(src,[]).append(maxflow.Edge(src,SINK,prize_limits.get(src,1)))
 
     return graph
 
@@ -46,7 +62,7 @@ def get_max_bipartite_matching(graph):
 
     return prizes
 
-graph = get_graph("choices.csv")
+graph = get_graph("choices.csv",prize_info_filename="prize_info.csv")
 # maxflow.print_graph(graph)
 # print("-"*20)
 
